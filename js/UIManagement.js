4bebc24513e5b93856bcb1edb1ec09f39d3ce804
Saushthav Saxena <ssaxena3@binghamton.edu>
2019-05-10 03:12:05 -0400

    changed name

--- js/UIManagement.js
+++ js/UIManagement.js
@@ -14,7 +14,7 @@ UIManagement = {
     modeButtonClassEnum: {city: "btn btn-light", arbCenter: "btn btn-success", algCenter: "btn btn-danger"},
     modeButtonText: {city: "City mode", arbCenter: "Arb.Center mode", algCenter: "Alg.Center mode"},
     algoTypeButtonClassEnum: {kCenter: "btn btn-primary", centerOfGravity: "btn btn-success", lloyd: "btn btn-info"},
-    algoTypeButtonText: {kCenter: "K-Center", centerOfGravity: "Center Of Gravity", lloyd: "lloyd"},
+    algoTypeButtonText: {kCenter: "K-Center", centerOfGravity: "Center Of Gravity", lloyd: "lloyd-kmeans"},
 
     getUIreferences: async function () {
         return new Promise((resolve, reject) => {
